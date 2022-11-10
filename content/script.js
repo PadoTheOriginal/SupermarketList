f9d283da4f19c99a6691b3578c18a77dd1419aad
PadoTheOriginal <pado30756@gmail.com>
2022-11-10 01:02:10 -0300

    Incresed interval check time, now adding and changing items does not reload the page

--- content/script.js
+++ content/script.js
@@ -1,6 +1,26 @@
 $(function () {
-    $('input[name="Quantity"]').on("keyup change", function () {
+    // Makes sure the quantity can never be less than one
+    $('input[name="Quantity"]').on("change", function () {
         if ($(this).val() < 1) $(this).val(1);
+    });
+
+    // Makes sure the price can never be less than zero
+    $('input[name="Price"]').on("keyup change", function () {
+        if ($(this).val() < 0) $(this).val(0);
+    });
+
+    // Saves new-item input value on local storage on change
+    $('.new-item').on("keyup change", function () {
+        let name = $(this).attr("name");
+        localStorage.setItem(`new-item-${name}`, $(this).val());
+    });
+
+    // Load input values if there are any saved
+    $('.new-item').each(function (i, element) {
+        let name = $(this).attr("name");
+        let item_value = localStorage.getItem(`new-item-${name}`);
+
+        if (item_value !== null) $(element).val(item_value);
     });
 });
 
@@ -8,13 +28,13 @@ function newItem() {
     let data = {};
     let valid = true;
 
-    $('.new-item').each(function (i, e) {
-        if ($(e).val() === '') {
-            $(e).focus();
+    $('.new-item').each(function (i, element) {
+        if ($(element).val() === '') {
+            $(element).focus();
             valid = false;
         }
 
-        data[$(e).attr("name")] = $(e).val();
+        data[$(element).attr("name")] = $(element).val();
     });
 
     if (valid == false) return 0;
@@ -25,7 +45,61 @@ function newItem() {
         async: true,
         data: data,
         dataType: "json",
-        success: function () { window.location.reload() },
+        success: function (obj) {
+            if (obj.success === true) {
+                supermarket_list_version = obj.version;
+                localStorage.clear();
+
+                let index = 1;
+
+                $('input[name="Index"]').each(function (i, element) {
+                    index = $(element).val() > index ? $(element).val() : index;
+                });
+
+                let htmlTR = `<tr>
+                                <td class="d-flex">
+                                    <input type="hidden" name="Index" value="${index}">
+                                    <input class="form-control item w-100 input-with-btn" type="text"
+                                        placeholder="Item" name="Name" value="${obj.supermarket_item.Name}"
+                                        onchange="changeItem(this)">
+                                    <button class="btn btn-danger input-btn" type="button"
+                                        onclick="removeItem(this)">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                </td>
+                                <td>
+                                    <input class="form-control item text-end" type="number" placeholder="Quantity"
+                                        name="Quantity" value="${obj.supermarket_item.Quantity}" onchange="changeItem(this)">
+                                </td>
+                                <td>
+                                    <input class="form-control item text-end" type="number" placeholder="Price"
+                                        name="Price" value="${obj.supermarket_item.Price}" onchange="changeItem(this)">
+                                </td>
+                                <td class="text-center align-middle total-item-price">
+                                    ${obj.supermarket_item.TotalFormat}
+                                </td>
+                            </tr>`;
+
+                
+                $('.new-items').before($(htmlTR));
+
+                $('.new-item').val(null);
+                $('input[name="Quantity"].new-item').val(1);
+
+                // Makes sure the quantity can never be less than one
+                $('input[name="Quantity"]').on("change", function () {
+                    if ($(this).val() < 1) $(this).val(1);
+                });
+
+                // Makes sure the price can never be less than zero
+                $('input[name="Price"]').on("keyup change", function () {
+                    if ($(this).val() < 0) $(this).val(0);
+                });
+
+
+                $('.total-price').text(`Total: ${obj.total_formatted}`);
+            }
+        },
         error: function (obj) {
             console.log(obj);
         }
@@ -40,13 +114,19 @@ function changeItem(element) {
 
     data["Index"] = $(parent).find('>:first-child>input[name="Index"]').val();
 
-    $(parent).find('.item').each(function (i, e) {
-        if ($(e).val() === '') {
-            $(e).focus();
+    $(parent).find('.item').each(function (i, element) {
+        let element_name = $(element).attr("name");
+
+        if ($(element).val() === '') {
+            $(element).focus();
             valid = false;
         }
+        
+        if ($(element).val() < 1 && element_name === "Quantity") $(element).val(1);
 
-        data[$(e).attr("name")] = $(e).val();
+        if ($(element).val() < 0 && element_name === "Price") $(element).val(0);
+
+        data[element_name] = $(element).val();
     });
 
     if (valid == false) return 0;
@@ -58,10 +138,15 @@ function changeItem(element) {
         data: data,
         dataType: "json",
         success: function (obj) {
-            window.location.reload();
+            if (obj.success === true) {
+                supermarket_list_version = obj.version;
+
+                $(parent).find('.total-item-price').text(obj.supermarket_item.TotalFormat);
+                $('.total-price').text(`Total: ${obj.total_formatted}`);
+            }
         },
         error: function (obj) {
-            console.log(obj);
+            alert('Error');
         }
     });
 
@@ -89,14 +174,15 @@ function removeItem(element) {
 }
 
 // just so I can have my supermarket list synced between multiple devices (Super important!!)
-function checkForUpdate(version) {
+function checkForUpdate() {
+
     $.ajax({
         url: "/GetVersion",
         type: "Get",
-        data: {version},
+        data: { supermarket_list_version },
         async: true,
         success: function (obj) {
-            if (obj.version != version) window.location.reload();
+            if (obj.version != supermarket_list_version) window.location.reload();
         }
     });
 }
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
-import pickle, os
+import pickle
+import os
 
-app = Flask("SupermaketList", template_folder="./content", static_folder="./content")
+app = Flask("SupermaketList", template_folder="./content",
+            static_folder="./content")
 supermarket_list = []
 version = 0
 users = []
@@ -11,34 +13,17 @@ multiple_users_interval = False
 @app.route("/")
 def index():
     global version
-    interval_delay = 6000
+    interval_delay = 2000 if multiple_users_interval else 10000
 
-    # only allow constant version check if there are multiple users
-    if multiple_users_interval:
-        interval_delay = 2000
+    total = sum([item["Total"] for item in supermarket_list])
+    total_formatted = 'R${:,.2f}'.format(total)
 
-    total = 'R${:,.2f}'.format(sum([item["Total"] for item in supermarket_list]))
-
-    return render_template("index.jinja2", supermarket_list=supermarket_list, total=total, version=version, interval_delay=interval_delay)
-
-
-@app.route("/GetVersion/", methods=['GET'])
-def get_version():
-    global multiple_users_interval
-    global version
-
-    # print(request.headers)
-
-    if (request.environ['REMOTE_ADDR'] in users):
-        users.remove(request.environ['REMOTE_ADDR'])
-
-    users.append(request.environ['REMOTE_ADDR'])
-
-    if len(users) > 1 and not multiple_users_interval:
-        multiple_users_interval = True
-        version += 1
-
-    return jsonify(success=True, version=version)
+    return render_template("index.jinja2",
+                           supermarket_list=supermarket_list,
+                           total_formatted=total_formatted,
+                           total=total,
+                           version=version,
+                           interval_delay=interval_delay)
 
 
 @app.route('/NewItem/', methods=['POST'])
@@ -49,15 +34,19 @@ def new_item():
     supermarket_item["Name"] = request.form["Name"]
     supermarket_item["Quantity"] = int(request.form["Quantity"])
     supermarket_item["Price"] = float(request.form['Price'])
-    supermarket_item["Total"] = supermarket_item["Quantity"] * supermarket_item["Price"]
-    supermarket_item["TotalFormat"] = 'R${:,.2f}'.format(supermarket_item["Total"])
+    supermarket_item["Total"] = supermarket_item["Quantity"] * \
+        supermarket_item["Price"]
+    supermarket_item["TotalFormat"] = 'R${:,.2f}'.format(
+        supermarket_item["Total"])
 
     supermarket_list.append(supermarket_item)
 
     pickle.dump(supermarket_list, open('supermarket_list.pickle', "wb"))
     version += 1
+    total = sum([item["Total"] for item in supermarket_list])
+    total_formatted = 'R${:,.2f}'.format(total)
 
-    return jsonify(success=True)
+    return jsonify(success=True, version=version, supermarket_item=supermarket_item, total_formatted=total_formatted)
 
 
 @app.route('/ChangeItem/', methods=['POST'])
@@ -69,13 +58,17 @@ def change_item():
     supermarket_list[index]["Name"] = request.form["Name"]
     supermarket_list[index]["Quantity"] = int(request.form["Quantity"])
     supermarket_list[index]["Price"] = float(request.form['Price'])
-    supermarket_list[index]["Total"] = supermarket_list[index]["Quantity"] * supermarket_list[index]["Price"]
-    supermarket_list[index]["TotalFormat"] = 'R${:,.2f}'.format(supermarket_list[index]["Total"])
+    supermarket_list[index]["Total"] = supermarket_list[index]["Quantity"] * \
+        supermarket_list[index]["Price"]
+    supermarket_list[index]["TotalFormat"] = 'R${:,.2f}'.format(
+        supermarket_list[index]["Total"])
 
     pickle.dump(supermarket_list, open('supermarket_list.pickle', "wb"))
     version += 1
+    total = sum([item["Total"] for item in supermarket_list])
+    total_formatted = 'R${:,.2f}'.format(total)
 
-    return jsonify(success=True)
+    return jsonify(success=True, supermarket_item=supermarket_list[index], total_formatted=total_formatted, version=version)
 
 
 @app.route('/RemoveItem/', methods=['POST'])
@@ -99,6 +92,23 @@ def add_header(r):
     r.headers["Expires"] = "0"
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
+
+
+@app.route("/GetVersion/", methods=['GET'])
+def get_version():
+    global multiple_users_interval
+    global version
+
+    if (request.environ['REMOTE_ADDR'] in users):
+        users.remove(request.environ['REMOTE_ADDR'])
+
+    users.append(request.environ['REMOTE_ADDR'])
+
+    if len(users) > 1 and not multiple_users_interval:
+        multiple_users_interval = True
+        version += 1
+
+    return jsonify(success=True, version=version)
 
 
 if __name__ == "__main__":
